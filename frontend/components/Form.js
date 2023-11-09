1771df2793c27805831b2ed36b6ebf80417bced7
Syed Ali <126282962+mrzexii@users.noreply.github.com>
2023-11-09 09:42:30 -0800

    Update Form.js

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,11 +1,39 @@
-import React from 'react'
+// Form.js
 
+import React from 'react';
+
+// This component represents a form for adding new Todos
 export default class Form extends React.Component {
+  state = {
+    name: '',
+  };
+
+  // Handles form submission
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.addTodo(this.state.name);
+    // Resetting the name input field after submission
+    this.setState({
+      name: '',
+    });
+  };
+
+  // Handles input field changes
+  onChange = (e) => {
+    const { value } = e.target;
+    this.setState({
+      name: value,
+    });
+  };
+
   render() {
     return (
-      <div>
-        Form
-      </div>
-    )
+      <form onSubmit={this.onSubmit}>
+        {/* Input field for Todo name */}
+        <input type="text" value={this.state.name} onChange={this.onChange} />
+        {/* Submit button */}
+        <input type="submit" />
+      </form>
+    );
   }
 }
