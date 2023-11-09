89a2021e0647a0c0916d1cdadd2bd35e75e2c5ac
Syed Ali <126282962+mrzexii@users.noreply.github.com>
2023-11-09 09:42:40 -0800

    Update Todo.js

--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -1,11 +1,19 @@
-import React from 'react'
+// Todo.js
 
+import React from 'react';
+
+// This component represents an individual Todo item
 export default class Todo extends React.Component {
   render() {
+    // Extracting properties from props
+    const { id, name, completed } = this.props.todo;
+    const { toggleCompletion } = this.props;
+
     return (
-      <div>
-        Todo
+      // Clicking on the Todo item triggers the toggleCompletion function with the Todo's id
+      <div onClick={() => toggleCompletion(id)}>
+        {name} {completed && '✓'}
       </div>
-    )
+    );
   }
 }
