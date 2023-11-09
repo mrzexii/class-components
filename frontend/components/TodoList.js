eaf10530d10cdc651ada5445cd53e30de6c28ff2
Syed Ali <126282962+mrzexii@users.noreply.github.com>
2023-11-09 09:42:52 -0800

    Update TodoList.js

--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -1,11 +1,40 @@
-import React from 'react'
+// TodoList.js
 
+import React from 'react';
+import Todo from './Todo';
+
+// This component represents a list of Todos
 export default class TodoList extends React.Component {
+  state = {
+    showAll: true,
+  };
+
+  // Toggles the visibility of completed Todos
+  toggleShowAll = () => {
+    this.setState({
+      ...this.state,
+      showAll: !this.state.showAll,
+    });
+  };
+
   render() {
+    // Extracting properties from props
+    const { todos, toggleCompletion } = this.props;
+
+    // Filtering Todos based on completion status and showAll state
+    const filtered = todos.filter((td) => !td.completed || this.state.showAll);
+
     return (
       <div>
-        TodoList
+        {/* Mapping over filtered Todos and rendering Todo components */}
+        {filtered.map((todo) => (
+          <Todo key={todo.id} todo={todo} toggleCompletion={toggleCompletion} />
+        ))}
+        {/* Button to toggle visibility of completed Todos */}
+        <button onClick={this.toggleShowAll}>
+          {this.state.showAll ? 'Hide Completed Todos' : 'Show All'}
+        </button>
       </div>
-    )
+    );
   }
 }
